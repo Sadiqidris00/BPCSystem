2806d34e54054f6546a33d8727a96efc59c5013a
Sadiqidris00 <isadiq845@gmail.com>
2025-03-30 22:55:39 +0100

    Added the Specialist Class

--- src/bcpsystem/Specialist.java
+++ src/bcpsystem/Specialist.java
@@ -1,13 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package bcpsystem;
+import java.util.ArrayList;
+import java.util.List;
 
-/**
- *
- * @author Office
- */
 public class Specialist {
-    
+    private String specialistName;
+    private String focusArea;
+    private List<TherapySession> sessionList = new ArrayList<>();
+
+    public Specialist(String specialistName, String focusArea) {
+        this.specialistName = specialistName;
+        this.focusArea = focusArea;
+    }
+
+    public String getSpecialistName() { return specialistName; }
+    public String getFocusArea() { return focusArea; }
+    public List<TherapySession> getSessionList() { return sessionList; }
+    public void scheduleNewSession(TherapySession session) { sessionList.add(session); }
 }
